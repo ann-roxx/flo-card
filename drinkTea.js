523d373736fc49280e3a30d5581ddcc4f7a330d0
ann-roxx <pavlenkoanna4444@gmail.com>
2026-03-23 17:12:29 +0100

    work with class-constructor and methods

---
+++ drinkTea.js
@@ -0,0 +1,19 @@
+// От класса Drink создать наследников и присвоить 
+// им дополнительные уникальные свойства.
+import {Drink} from "./drink.js";
+
+export class Tea extends Drink {
+  constructor(name, size, price, type, strength, country, temperature) {
+    super(name, size, price)
+    this.type = type;
+    this.strength = strength;
+    this.country = country;
+
+    this.setTemperature(temperature);
+  }
+
+  getInfo() {
+    return [...super.getInfo(), this.type, this.strength, this.country];
+  }
+}
+
